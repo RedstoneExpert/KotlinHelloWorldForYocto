c28ecb6f0bbd056db5f8f3a9baac381555d64fc5
RedstoneExpert <lukavec.martin.iv@gmail.com>
2026-03-31 02:24:23 +0200

    Added DBus server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
 package com.closedbrain
 
-class Test
+import org.freedesktop.dbus.annotations.DBusInterfaceName
+import org.freedesktop.dbus.connections.impl.DBusConnectionBuilder
+import org.freedesktop.dbus.interfaces.DBusInterface
+
+@DBusInterfaceName("org.example.Calculator")
+interface Calculator : DBusInterface {
+    fun add(a: Int, b: Int): Int
+    fun getGreeting(name: String): String
+}
+
+class CalculatorImpl : Calculator {
+    override fun isRemote() = false
+    override fun getObjectPath() = "/org/example/Calculator"
+
+    override fun add(a: Int, b: Int): Int {
+        println("Server: Adding $a + $b")
+        return a + b
+    }
+
+    override fun getGreeting(name: String): String {
+        return "Hello, $name! Kotlin DBus is working."
+    }
+}
 
 fun main() {
-    println("Name teto classy je: " + Test::class.qualifiedName)
+    val connection = DBusConnectionBuilder.forSessionBus().build()
+
+    connection.requestBusName("org.example.CalculatorService")
+
+    val calc = CalculatorImpl()
+    connection.exportObject(calc.objectPath, calc)
+
+    Thread.sleep(Long.MAX_VALUE)
 }
